3a63f46f37103cc78820ed4a65ea7751f064f6ab
Saadman-Abedin <63958371+Saadman-Abedin@users.noreply.github.com>
2020-04-19 14:17:45 +0200

    when the final time(hour) will be negative
    # Task1047__________Game Time with Minutes
    
    st,sm,et,em= list(map(int,input().split()))
    ft = et - st
    if(ft < 0):
        ft = 24 + (et - st)
    fm = em - sm
    if(fm < 0):
         fm = 60 + (em - sm)
         ft=ft-1
         if ft<0:
             ft=24+ft
    if (et == st and em == sm):
        print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
    else:
        print("O JOGO DUROU { } HORA(S) E { } MINUTO(S)".format(ft,fm))

--- problem 1047 Game Time With Minutes.py
+++ problem 1047 Game Time With Minutes.py
@@ -6,6 +6,8 @@ fm = em - sm
 if(fm < 0):
      fm = 60 + (em - sm)
      ft=ft-1
+     if ft<0:
+         ft=24+ft
 if (et == st and em == sm):
     print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)")
 else:
